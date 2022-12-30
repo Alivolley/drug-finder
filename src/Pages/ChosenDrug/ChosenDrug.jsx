62884e86010d28cde3c82e75f050420739eedf31
Ali <alicryptovolley@gmail.com>
2022-12-30 22:50:36 +0330

    chosen drug section finished

--- src/Pages/ChosenDrug/ChosenDrug.jsx
+++ src/Pages/ChosenDrug/ChosenDrug.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import Header from "./../../Components/Header/Header";
+import SearchItem from "./../../Components/SearchItem/SearchItem";
 
 export default function ChosenDrug() {
-   return <div>ChosenDrug</div>;
+   return (
+      <ChosenDrugWrapper>
+         <Header />
+         <SearchItem />
+      </ChosenDrugWrapper>
+   );
 }
+
+const ChosenDrugWrapper = styled.section``;
